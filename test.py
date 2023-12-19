4243026981b09e20aa10420f7a383f24d2d240fe
mlz5080 <mlz961116@gmail.com>
2023-12-19 09:36:34 -0500

    2023-12-19 code changes

--- test.py
+++ test.py
@@ -11,8 +11,23 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import re
 
 domain = "https://www.costco.ca"
+
+class CostcoItem:
+    def __init__(self, item_id, name, price, price_range, hmp, is_on_sale, link):
+        self.id = item_id
+        self.price = price
+        self.price_range = price_range
+        self.history_minimum_price = hmp
+        self.name = name
+        self.link = link
+        self.is_on_sale = is_on_sale
+
+    def __str__(self):
+        return " ".join([self.id, self.name, self.price, "is on sale" if self.is_on_sale else "not on sale", self.link])
+
 
 def get_shop_by_category_links(link):
     # res = requests.get(link)
@@ -36,7 +51,6 @@ def get_shop_by_category_links(link):
     for cookie in driver.get_cookies():
         s.cookies.set(cookie['name'], cookie['value'], domain=cookie['domain'])
     response = s.get("https://www.costco.ca/everlast-1910-classic-training-leather-glove-kit.product.1468886.html")
-    # print(response.content)
     sauce = BeautifulSoup(response.content, "lxml")
     print(sauce.find("div", id="pull-right-price"))
     return level1_html
@@ -57,10 +71,6 @@ def get_driver():
 def get_url_from_site_map():
     url = "https://www.costco.ca/SiteMapDisplayView"
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
-    # s = requests.Session()
-    # with requests.Session() as s:
-    #     s.headers.update({"user-agent": user_agent})
-    #     response = s.get(url)
     driver = get_driver()
     driver.get(url)
     # time.sleep(1)
@@ -68,26 +78,45 @@ def get_url_from_site_map():
     # print(sauce)
     site_map_div = sauce.find("div", class_="costcoBD-sitemap")
     all_sites = site_map_div.find_all('a', href=True)
-    for a in all_sites:
-        print(a.string.strip(), a.get("href"))
-    get_product("https://www.costco.ca/learning-systems.html")
+    # for a in all_sites:
+    #     print(a.string.strip(), a.get("href"))
+    get_product("https://www.costco.ca/coffee-tea.html")
     # driver.close()
 
-def retrive_name_and_links(div):
-    sub_a = div.find_parent().find_next_sibling().a
-    return (sub_a.string.strip(), sub_a.get('href'))
+def retrive_product_info(div):
+    product_price = div.string.strip()
+    item_id = re.findall(r'\d+',div.get("id").strip())[0]
+    div_parent = div.find_parent()
+    span = div_parent.find_next_sibling("span")
+    # print("instantSavings" in div_parent.find_next_sibling("p").get("automation-id"))
+
+    is_on_sale = False
+
+    if div_parent.find_next_sibling("p") and div_parent.find_next_sibling("p").has_attr("automation-id"):
+        is_on_sale = "instantSavings" in div_parent.find_next_sibling("p").get("automation-id")
+    sub_a = span.find("a")
+    product_name = sub_a.string.strip()
+    product_link = sub_a.get('href')
+    
+    return CostcoItem(item_id, product_name, product_price, is_on_sale, product_link)
 
 def get_product(url):
     driver = get_driver()
-    driver.get(url)
-    sauce = BeautifulSoup(driver.page_source, "lxml")
-    # product_List = sauce.find("div", {"automation-id":"productList"})
-    price_divs = sauce.find_all("div", {"class": "price"})
-    price_list = [pd.string.strip() for pd in price_divs]
-    product_info = [retrive_name_and_links(pd) for pd in price_divs]
 
-    for price, info in zip(price_list, product_info):
-        print(info[0], price, info[1])
+    has_next_page = True
+    while has_next_page:
+        driver.get(url)
+        sauce = BeautifulSoup(driver.page_source, "lxml")
+        price_divs = sauce.find_all("div", {"class": "price"})
+        # print(price_divs)
+        for pd in price_divs:
+            if not pd.string:
+                print(pd.contents)
+        product_info = [retrive_product_info(pd) for pd in price_divs]
+        for product_info in product_info:
+            print(product_info)
+        print(sauce.find("li", class_="forward"))
+        break
     driver.close()
 
 if __name__ == '__main__':
