da3a1d20159d85da28913977e27b4152e6488bf6
mlz5080 <mlz961116@gmail.com>
2023-12-20 18:09:25 -0500

    retrieve single source

--- test.py
+++ test.py
@@ -12,21 +12,36 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import re
+import boto3
 
-domain = "https://www.costco.ca"
+costco_domain = "https://www.costco.ca"
+costco_db_table_name = "costco-products-info"
+
 
 class CostcoItem:
-    def __init__(self, item_id, name, price, price_range, hmp, is_on_sale, link):
+    def __init__(self, item_id, name, price, price_range, is_on_sale, product_link, image_link):
         self.id = item_id
         self.price = price
         self.price_range = price_range
-        self.history_minimum_price = hmp
         self.name = name
-        self.link = link
+        self.link = product_link
         self.is_on_sale = is_on_sale
+        self.image_link = image_link
 
     def __str__(self):
-        return " ".join([self.id, self.name, self.price, "is on sale" if self.is_on_sale else "not on sale", self.link])
+        return " ".join(
+            [
+                self.id,
+                self.name,
+                self.price,
+                "is on sale" if self.is_on_sale else "not on sale",
+                self.link,
+                self.image_link,"\n"
+            ]
+        )
+
+    def to_json(self):
+        return 
 
 
 def get_shop_by_category_links(link):
@@ -64,6 +79,7 @@ def get_driver():
         chromeOptions = webdriver.ChromeOptions()
         chromeOptions.add_argument("--headless")
         chromeOptions.add_argument('user-agent={0}'.format(user_agent))
+        chromeOptions.add_argument("--log-level=3")
         driver = webdriver.Chrome(options=chromeOptions)
         setattr(threadLocal, 'driver', driver)
     return driver
@@ -73,53 +89,68 @@ def get_url_from_site_map():
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
     driver = get_driver()
     driver.get(url)
-    # time.sleep(1)
     sauce = BeautifulSoup(driver.page_source, "lxml")
-    # print(sauce)
     site_map_div = sauce.find("div", class_="costcoBD-sitemap")
     all_sites = site_map_div.find_all('a', href=True)
-    # for a in all_sites:
-    #     print(a.string.strip(), a.get("href"))
-    get_product("https://www.costco.ca/coffee-tea.html")
-    # driver.close()
+    all_sites = [costco_domain+a.get("href") for a in all_sites]
+    return all_sites[:2]
 
-def retrive_product_info(div):
-    product_price = div.string.strip()
+def retrive_product_info(div, sauce):
+    product_price_str = div.string
+    # item_id, name, price, price_range, hmp, is_on_sale, link
+    price_range = ""
+    if not product_price_str:
+        product_price, price_range = div.contents[0].strip(), div.contents[-1].strip()
+    else:
+        product_price = div.string.strip()
     item_id = re.findall(r'\d+',div.get("id").strip())[0]
     div_parent = div.find_parent()
     span = div_parent.find_next_sibling("span")
-    # print("instantSavings" in div_parent.find_next_sibling("p").get("automation-id"))
-
     is_on_sale = False
-
     if div_parent.find_next_sibling("p") and div_parent.find_next_sibling("p").has_attr("automation-id"):
         is_on_sale = "instantSavings" in div_parent.find_next_sibling("p").get("automation-id")
     sub_a = span.find("a")
     product_name = sub_a.string.strip()
     product_link = sub_a.get('href')
-    
-    return CostcoItem(item_id, product_name, product_price, is_on_sale, product_link)
+    image_link = sauce.find("img", {"alt": product_name}).get("src")
+    if not image_link:
+        image_link = sauce.find("img", {"alt": product_name}).get("data-src")
+    # print(sauce.find("img", {"alt": product_name}))
+    return CostcoItem(item_id, product_name, product_price, price_range, is_on_sale, product_link, image_link)
 
-def get_product(url):
+def db_update(costco_product, table_name):
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table(table_name)
+
+def get_costco_product(url):
+    print("#"*10, url, "#"*10)
     driver = get_driver()
-
     has_next_page = True
+    page = 1
+    driver.get(url)
     while has_next_page:
-        driver.get(url)
         sauce = BeautifulSoup(driver.page_source, "lxml")
         price_divs = sauce.find_all("div", {"class": "price"})
-        # print(price_divs)
-        for pd in price_divs:
-            if not pd.string:
-                print(pd.contents)
-        product_info = [retrive_product_info(pd) for pd in price_divs]
+        if not price_divs:
+            print("errors")
+            return
+        product_info = [retrive_product_info(pd, sauce) for pd in price_divs]
+        print("\nProduct Page {}\n".format(page))
+        # with open("{}.txt".format(url))
         for product_info in product_info:
             print(product_info)
-        print(sauce.find("li", class_="forward"))
-        break
+            # db_update()
+        # print("#"*100)
+        # print()
+        has_next_page = sauce.find("li", class_="forward") != None
+        page = page+1 if has_next_page else page
+        try:
+            driver.execute_script("arguments[0].click();", WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "i[automation-id='nextPageNavigationLink']"))))
+        except:
+            print("Done")
     driver.close()
 
 if __name__ == '__main__':
-    get_url_from_site_map()
+    # get_url_from_site_map()
     # get_shop_by_category_links(domain)
-    # Pool(os.cpu_count()).map(get_title, get_shop_by_category_links(domain))
+    Pool(1).map(get_costco_product, get_url_from_site_map())
